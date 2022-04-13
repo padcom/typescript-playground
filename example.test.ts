fc7c1fe0bed0e963a664aca878df30b21b75962f
Matthias Hryniszak <padcom@gmail.com>
2022-04-13 22:41:41 +0200

    Initial import

---
+++ example.test.ts
@@ -0,0 +1,5 @@
+describe('Subsystem', () => {
+  it('works', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ main.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S npx ts-node
+
+import Person from './lib/Person'
+
+console.log(new Person('John', 'Doe'))
